1dbc9f283c9acfd9d75e622a03e7e987bf39f9fd
Sayandeep Karak <sayandeep18.webdev@gmail.com>
2026-05-12 20:13:34 +0530

    feat: added antd and zustand

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import { Button, Result } from "antd";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Link href="/">
+            <Button type="primary">Back Home</Button>
+          </Link>
+        }
+      />
+    </div>
+  );
+}
